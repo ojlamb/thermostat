46dceef7ba855224c83a8b1e39ea6132d092898d
Makers Laptop 22 <makerslaptop22@Makerss-MacBook-Pro-3.local>
2015-08-19 17:00:01 +0100

    city text submission works

--- src/weather.js
+++ src/weather.js
@@ -1,19 +1,26 @@
 $(document).ready(function(){
 
-  $(".get_weather").click(function(){
-    weatherInfo();
+  $(".getWeather").click(function(){
+    showCity();
+   weatherInfo();
   });
 
+  function showCity(){
+    var city = $("#city").val();
+    $(".city").text(city);
+  };
+
   function showWeather(info) {
-    // ("City: " + info.city.name);
-    $(".weather").text(info.weather[0].description);
+      $(".weather").text(info.list[0].weather[0].description);
   };
 
   function weatherInfo() {
-  $.getJSON("http://api.openweathermap.org/data/2.5/forecast/weather?id=524901&APPID={0104110a44375e2dda55ca11b2fb9353}", function(result){
-    showWeather(result);
-  })
-  };
+    var city = $("#city").val();
+    // $(".city").text(city);
+    $.getJSON("http://api.openweathermap.org/data/2.5/forecast/daily?q="+city+"&mode=json&units=metric&cnt=10",function(result){
+            showWeather(result);
+            });
+        };
 
 
 });
